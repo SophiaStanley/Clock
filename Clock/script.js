8048f5abfa0b08467de1b0e2cff04f2c87434b5c
Soph <C00299461@setu.ie>
2025-09-29 10:22:36 +0100

    The starting part I forgot to commit earlier

---
+++ Clock/script.js
@@ -0,0 +1,16 @@
+
+// currently, most of this code, including the html and css, are taken from Colm's code. Thank you Colm
+// https://gitlab.com/colmoneill/creative-studio-code-samples/-/tree/main/01_JS-clock?ref_type=heads
+
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var operand = 4;
+    document.getElementById("main").innerHTML = hour + " : " + min + " : " + sec;
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+currentTime()
+
